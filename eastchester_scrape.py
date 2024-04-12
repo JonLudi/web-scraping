dd0084d9c5ffe3dbcfbadd37b0113e9fd8e0c5fd
JonLudi <144353376+JonLudi@users.noreply.github.com>
2024-04-12 14:14:58 +0000

    Hacking Eastchester Bums

---
+++ eastchester_scrape.py
@@ -0,0 +1,7 @@
+import requests
+import bs4 import BeautifulSoup
+import codecs
+import re
+
+eastchester_scrape = requests.get("https.get//www.eufsdk12.org/domain/76")
+soup = BeautifulSoup(estchester_scrape.text, "html.parser")
